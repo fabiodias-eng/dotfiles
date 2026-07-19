489d88bed6413e12188609c0d8fdf5c36a645bdb
Fabio Dias <dias.c.fabio@gmail.com>
2026-07-19 21:04:34 +0000

    chore: update treesitter configuration and Lazy initialization

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,24 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 require("config.lazy")
 require("config.keymaps")
+require("config.options")
+
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "c", "cpp", "python", "lua"},
+    pattern = {
+        "bash",
+        "c",
+        "cpp",
+        "cmake",
+        "lua",
+        "python",
+    },
     callback = function()
         vim.treesitter.start()
     end,
 })
+
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,36 @@ return {
 	{
   		'nvim-treesitter/nvim-treesitter',
   		lazy = false,
-  		build = ':TSUpdate'
-	}
+  		build = ':TSUpdate',
+
+        opts = {
+            ensure_installed = {
+                "bash",
+                "c",
+                "cpp",
+                "cmake",
+                "diff",
+                "dockerfile",
+                "git_config",
+                "git_rebase",
+                "gitcommit",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "toml",
+                "vim",
+                "vimdoc",
+                "xml",
+                "yaml",
+            },
+
+            auto_install = true,
+
+            highlight = {
+                enable = true,
+            },
+        },
+    },
 }
