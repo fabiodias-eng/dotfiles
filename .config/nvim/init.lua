8aace43f298a5c576c882d4a74b92cec2de53a89
Fabio Dias <137723212+fabiodias-eng@users.noreply.github.com>
2026-09-08 22:37:46 -0300

    Update nvim lazy-lock and fix macro recording not showing up

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,11 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 require("config.options")
 require("config.lazy")
 require("config.keymaps")
 require("config.filetypes")
 require("config.autocmds")
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
 
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -18,3 +18,15 @@ vim.api.nvim_create_autocmd("CmdlineLeave", {
 		vim.opt.cmdheight = 0
 	end,
 })
+
+vim.api.nvim_create_autocmd("RecordingEnter", {
+	callback = function()
+		vim.opt.cmdheight = 1
+	end,
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+	callback = function()
+		vim.opt.cmdheight = 0
+	end,
+})
